521592ab4ba885483fe0c0c585c0cf31774cccd7
Balthazar Rouberol <balto@Balto-Latitude.(none)>
2011-06-02 22:50:24 +0200

    If no pictures, don't run

--- resize.sh
+++ resize.sh
@@ -2,33 +2,41 @@
 
 # --- This script applies a scale-resize function to all pictures in the same directory ---
 
-# Dialog box to choose thumb's size
-SIZE=`zenity --scale --text="Choose scale of resized images" --min-value=1 --max-value=100 --step=1 --value=50`
+# Check for arguments	
+if [ $# = 1 ]; then
+	DIR="$1"
+else
+	DIR="./"
+fi
 
-# How many files to make the progress bar
+#Init variables
+PROGRESS=0
+FILES=`ls $DIR | egrep '(.JPG)|(.jpg)|(.png)|(.raw)|(.tiff)'`
+NB_FILES=`ls $DIR | egrep '(.JPG)|(.jpg)|(.png)|(.raw)|(.tiff)' | wc -l`
 
-if [ "$SIZE" != "" ]; then
-	
-	PROGRESS=0
-	if [ $# = 1 ]; then
-		DIR="$1"
-	else
-		DIR="./"
+# If picture files in directory
+if [ $NB_FILES -ne 0 ]; then
+
+	# Dialog box to choose thumb's size
+	SIZE=`zenity --scale --text="Choose scale of resized images" --min-value=1 --max-value=100 --step=1 --value=50`
+
+	if [ "$SIZE" != "" ]; then # If user haven't clicked on 'Cancel'
+
+		INCREMENT=$[100/$NB_FILES]
+		mkdir -p $DIR"Resized"
+
+		# Creating thumbnails. Specific work on picture should be add there as convert's option
+		(for i in $FILES; do
+		    echo "$PROGRESS"; # a line with no # updates the percentage
+		    echo "# Resizing $i" # a line beginning with # updates the text field
+		    convert -resize "$SIZE%" "$DIR${i}" "$DIR"Resized/"${i}"
+		    let "PROGRESS+=$INCREMENT"
+		done
+		) | zenity --progress --percentage=0 --auto-close --title="Creating thumbnails..."
+
 	fi
 
-	FILES=`ls $DIR | egrep '(.JPG)|(.jpg)|(.png)|(.raw)|(.tiff)'`
-	NB_FILES=`ls $DIR | egrep '(.JPG)|(.jpg)|(.png)|(.raw)|(.tiff)' | wc -l`
-	INCREMENT=$[100/$NB_FILES]
-
-	mkdir -p $DIR"Resized"
-
-	# Creating thumbnails. Specific work on picture should be add there as convert's option
-	(for i in $FILES; do
-	    echo "$PROGRESS"; # a line with no # updates the percentage
-	    echo "# Resizing $i" # a line beginning with # updates the text field
-	    convert -resize "$SIZE%" "$DIR${i}" "$DIR"Resized/"${i}"
-	    let "PROGRESS+=$INCREMENT"
-	done
-	) | zenity --progress --percentage=0 --auto-close --title="Creating thumbnails..."
+else
+	echo "No image files in $DIR directory"
 
 fi
