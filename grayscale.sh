90e4d6c822ee1fd97f84bbcbea302efedc198a89
Balthazar Rouberol <balto@Balto-Latitude.(none)>
2011-06-02 22:53:01 +0200

    If no pictures, don't run

--- grayscale.sh
+++ grayscale.sh
@@ -2,27 +2,34 @@
 
 # --- This script applies a black&white conversion function to all pictures in the same directory ---
 
-# How many files to make the progress bar
-PROGRESS=0
+
 if [ $# = 1 ]; then
 	DIR="$1"
 else
 	DIR="./"
 fi
-SUBDIR="GrayScale/"
 
+SUBDIR="GrayScale/"
+PROGRESS=0
 FILES=`ls $DIR | egrep '(.JPG)|(.jpg)|(.png)|(.raw)|(.tiff)'`
 NB_FILES=`ls $DIR | egrep '(.JPG)|(.jpg)|(.png)|(.raw)|(.tiff)' | wc -l`
-INCREMENT=$[100/$NB_FILES]
 
-mkdir -p $DIR$SUBDIR
+if [ $NB_FILES -ne 0 ]; then
 
-# Creating thumbnails. Specific work on picture should be add there as convert's option
-(for i in $FILES; do
-    echo "$PROGRESS"; # a line with no # updates the percentage
-    echo "# Converting $i to grayscale" # a line beginning with # updates the text field
+	INCREMENT=$[100/$NB_FILES]
 
-    convert  "$DIR${i}" -colorspace Gray "$DIR$SUBDIR${i}"
-    let "PROGRESS+=$INCREMENT"
-done
-) | zenity --progress --percentage=0 --auto-close --title="Creating thumbnails..."
+	mkdir -p $DIR$SUBDIR
+
+	# Creating thumbnails. Specific work on picture should be add there as convert's option
+	(for i in $FILES; do
+	    echo "$PROGRESS"; # a line with no # updates the percentage
+	    echo "# Converting $i to grayscale" # a line beginning with # updates the text field
+
+	    convert  "$DIR${i}" -colorspace Gray "$DIR$SUBDIR${i}"
+	    let "PROGRESS+=$INCREMENT"
+	done
+	) | zenity --progress --percentage=0 --auto-close --title="Creating thumbnails..."
+
+else
+	echo "No picture files in $DIR directory."
+fi
